7d493635ba96a106cac35a0f32094338688930b3
aguiluxo91 <apuertadelaguila@gmail.com>
2021-09-24 16:29:37 +0200

    footer first styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,30 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Footer = () => {
     return (
         <footer>
-            <p>© Alejandro Puerta 2021</p>
-            <p>Built with Next.js</p>
+            <div className="footer-name">
+                <p>© Alejandro Puerta 2021</p>
+                <p>Built with Next.js</p>
+            </div>
+            <div className="footer-icons">
+                <Link href="mailto:apuertadelaguila@gmail.com">
+                    <a className="icon">
+                        <Image src="/mail.svg" alt="mail" width="25" height="25" />
+                    </a>
+                </Link>
+                <Link href="https://www.linkedin.com/in/alejandro-puerta-17b404201" >
+                    <a className="icon" target="_blank">
+                        <Image src="/linkedin.svg" alt="linkedin" width="25" height="25" />
+                    </a>
+                </Link>
+                <Link href="https://github.com/aguiluxo91">
+                    <a className="icon">
+                        <Image src="/github.svg" alt="linkedin" width="25" height="25" />
+                    </a>
+                </Link>
+            </div>
         </footer>
     );
 }
