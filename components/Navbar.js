4890fd95c5981253f95c2237fb8d367d4453600c
aguiluxo91 <apuertadelaguila@gmail.com>
2021-09-25 16:11:46 +0200

    refactor styles from bootstrap to normal css

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,41 +1,42 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+
 const Navbar = () => {
 
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-      <nav className="row g-0">
-        <div className="col-md-8">
+  return (
+    <nav className="row g-0">
+      <div className="col-md-8">
+        <Link href="/">
+          Alejandro Puerta del Águila
+        </Link>
+      </div>
+      <ul className="col-md-4">
+        <li className={router.pathname == "/" ? "li-borders" : ""}>
           <Link href="/">
-            Alejandro Puerta del Águila
+            Home
           </Link>
-        </div>
-        <ul className="col-md-4">
-          <li className={router.pathname == "/" ? "border border-white" : ""}>
-            <Link href="/">
-              Home
-            </Link>
-          </li>
-          <li className={router.pathname == "/work" ? "border border-white" : ""}>
-            <Link href="/work">
-              Work
-            </Link>
-          </li>
-          <li className={router.pathname == "/about" ? "border border-white" : ""}>
-            <Link href="/about">
-              About
-            </Link>
-          </li>
-          <li className={router.pathname == "/contact" ? "border border-white" : ""}>
-            <Link href="/contact">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    );
+        </li>
+        <li className={router.pathname == "/work" ? "li-borders" : ""}>
+          <Link href="/work">
+            Work
+          </Link>
+        </li>
+        <li className={router.pathname == "/about" ? "li-borders" : ""}>
+          <Link href="/about">
+            About
+          </Link>
+        </li>
+        <li className={router.pathname == "/contact" ? "li-borders" : ""}>
+          <Link href="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
- 
+
 export default Navbar;
--- pages/work.js
+++ pages/work.js
@@ -1,32 +1,31 @@
-import Image from "next/image";
 
 const Work = () => {
     return (
         <div className="work">
             <h1>This is some of my Work</h1>
             <section className="projects">
-                <div className="project p-0 m-2">
+                <div className="project">
                     <img src="/SuperPang.png"/>
-                    <div className="bg-black p-1">
-                        <p className="text-white m-0">Super Pang</p>
+                    <div>
+                        <p>Super Pang</p>
                     </div>
                 </div>
-                <div className="project p-0-0 m-2">
+                <div className="project">
                     <img src="/JustGame.png"/>
-                    <div className="bg-black p-1">
-                        <p className="text-white m-0">Just Game</p>
+                    <div>
+                        <p>Just Game</p>
                     </div>
                 </div>
-                <div className="project p-0 m-2">
+                <div className="project">
                     <img src="/PetPortal.png"/>
-                    <div className="bg-black p-1">
-                        <p className="text-white m-0">Pet Portal</p>
+                    <div>
+                        <p>Pet Portal</p>
                     </div>
                 </div>
-                <div className="project p-0 m-2">
+                <div className="project">
                     <img src="/ThePhoneCave.png"/>
-                    <div className="bg-black p-1">
-                        <p className="text-white m-0">The Phone Cave</p>
+                    <div>
+                        <p>The Phone Cave</p>
                     </div>
                 </div>
             </section>
